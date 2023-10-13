0da2d898f48c0e31b81b81caad40da7f3120ca46
Colton Randall <coltonrandall.nz@gmail.com>
2023-10-13 15:37:08 +1300

    Initial commit - add basic functions

---
+++ logic.js
@@ -0,0 +1,15 @@
+function addNumbers(a, b){
+ return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
